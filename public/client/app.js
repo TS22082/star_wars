9aa39028ec1352646214f24c17d7eb797606ce61
TS22082 <ts22082@gmail.com>
2019-12-14 11:15:30 -0800

    newest push

--- public/client/app.js
+++ public/client/app.js
@@ -1,12 +1,35 @@
 $(document).ready(() => {
-  console.log("hello");
+  function displayCharacter(nm, rl, fp) {
+    $("#role-header").text(rl);
+    $("#name").text(nm);
+    $("#role").text(rl);
+    $("#forcePower").text(fp);
+  }
 
   $.ajax({
     type: "GET",
     url: "/api"
   }).then(res => {
-    console.log(res);
     const randNum = Math.floor(Math.random() * res.length);
-    console.log(res[randNum]);
+    const display = res[randNum];
+    displayCharacter(display.name, display.rank, display.forcepoints);
   });
+});
+
+$("#btnSearch").on("click", () => {
+  let userSearch = $("#search").val();
+  console.log(userSearch);
+  userSearch = userSearch
+    .split(" ")
+    .join("")
+    .toLowerCase();
+
+  $.ajax({
+    type: "GET",
+    url: `/api/${userSearch}`
+  }).then(res => {
+    console.log(res);
+  });
+
+  console.log(userSearch);
 });
