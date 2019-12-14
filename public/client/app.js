8bff95f093898e731ad54f53bd07e920fa8442b4
TS22082 <ts22082@gmail.com>
2019-12-14 12:54:38 -0800

    finished our star wars project

--- public/client/app.js
+++ public/client/app.js
@@ -1,10 +1,10 @@
 $(document).ready(() => {
-  function displayCharacter(nm, rl, fp) {
+  displayCharacter = (nm, rl, fp) => {
     $("#role-header").text(rl);
     $("#name").text(nm);
     $("#role").text(rl);
     $("#forcePower").text(fp);
-  }
+  };
 
   $.ajax({
     type: "GET",
@@ -14,22 +14,40 @@ $(document).ready(() => {
     const display = res[randNum];
     displayCharacter(display.name, display.rank, display.forcepoints);
   });
-});
 
-$("#btnSearch").on("click", () => {
-  let userSearch = $("#search").val();
-  console.log(userSearch);
-  userSearch = userSearch
-    .split(" ")
-    .join("")
-    .toLowerCase();
+  $("#btnSearch").on("click", () => {
+    let userSearch = $("#search").val();
+    userSearch = userSearch
+      .split(" ")
+      .join("")
+      .toLowerCase();
 
-  $.ajax({
-    type: "GET",
-    url: `/api/${userSearch}`
-  }).then(res => {
-    console.log(res);
+    $.ajax({
+      type: "GET",
+      url: `/api/${userSearch}`
+    }).then(res => {
+      console.log(res);
+      displayCharacter(res.name, res.rank, res.forcepoints);
+    });
   });
 
-  console.log(userSearch);
+  $("#btnSubmit").on("click", e => {
+    e.preventDefault();
+
+    const newChar = {
+      name: $("#nameInput").val(),
+      rank: $("#roleInput").val(),
+      forcepoints: $("#fpInput").val()
+    };
+
+    console.log(newChar);
+
+    $.ajax({
+      type: "POST",
+      url: "/api",
+      data: newChar
+    }).then(res => {
+      console.log(res);
+    });
+  });
 });
